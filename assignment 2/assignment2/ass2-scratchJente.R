98fb74ff197e30115c7a2652beb58a05fb3d987f
jsno <33056279+jsno@users.noreply.github.com>
2019-02-27 13:02:38 +0100

    rre

--- assignment 2/assignment2/ass2-scratchJente.R
+++ assignment 2/assignment2/ass2-scratchJente.R
@@ -1,1 +1,6 @@
 ### Scratchfile Jente (intermediate code or just shit you're trying goes here)
+library(ggplot2)
+ggplot(telco_train, aes(COUNT_CONNECTIONS_3MONTH, class=CHURN)) +
+       geom_histogram(position="identity", binwidth=3, aes(y=..density.., fill=CHURN),  alpha=0.5) +
+       geom_density()
+summary(telco_train)
